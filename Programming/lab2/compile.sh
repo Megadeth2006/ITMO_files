2ab465faee9ffaf91fb6dd562ef2cad46d2df723
Megadeth2006 <danielgrigorevdlol@gmail.com>
2024-10-24 01:03:02 +0300

    add README

--- Programming/lab2/compile.sh
+++ Programming/lab2/compile.sh
@@ -1,6 +1,5 @@
 javac -cp ./lib/Pokemon.jar -d bin -sourcepath src ./src/Main.java
 echo -e "Manifest-Version: 1.0\nMain-Class: Main\nClass-Path: lib/Pokemon.jar" > MANIFEST.MF
 jar -cfm main.jar MANIFEST.MF -C bin .
-mv main.jar ./optional_files
 rm MANIFEST.MF
 rm -rf bin
