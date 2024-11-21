c01746eff42ade1d811c2bb73f3f2b4c427dd338
Megadeth2006 <danielgrigorevdlol@gmail.com>
2024-11-21 08:50:44 +0300

    current commit

--- Programming/lab2/src/Main.java
+++ Programming/lab2/src/Main.java
@@ -7,21 +7,13 @@ import Pokemons.*;
 public class Main {
     public static void main(String[] args) {
         Battle b = new Battle();
-        List<Pokemon> pokemons = new ArrayList<>(List.of(new Fearow("Гигачад", 1), new Pichu("Мелочь Пузатая", 1), new Pikachu("Мышь", 1), new Raichu("Ноунейм", 1), new Spearow("Сигма", 1), new Victini("Инопланетянин", 1)));
-        Collections.shuffle(pokemons);
 
-        for(Pokemon p : pokemons){
-            System.out.printf( "[%d] %s %n", p.getLevel(), p.getClass().getSimpleName());
-        }
-
-        System.out.println();
-
-
-        for (int i = 0; i < pokemons.size(); i++)
-            if (i % 2 == 0)
-                b.addFoe(pokemons.get(i));
-            else
-                b.addAlly(pokemons.get(i));
+        b.addFoe(new Fearow("Гигачад", 1));
+        b.addAlly(new Pichu("Мелочь Пузатая", 1));
+        b.addFoe(new Pikachu("Мышь", 1));
+        b.addAlly(new Raichu("Ноунейм", 1));
+        b.addFoe(new Spearow("Сигма", 1));
+        b.addAlly(new Victini("Инопланетянин", 1));
 
         b.go();
 
