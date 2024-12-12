648f249fa12e2026f41c92f15b2ff1045c383431
Megadeth2006 <danielgrigorevdlol@gmail.com>
2024-12-13 01:12:09 +0300

    labwork by mathanalisis is done

--- MathAnalysis/Labwork1/main.py
+++ MathAnalysis/Labwork1/main.py
@@ -1,39 +1,38 @@
-import math
+from decimal import Decimal # импорт библиотеки для работы с большими числами
+
+import math # импорт библиотеки для использования математических функций
 def sign(x): # определение знака числа: -1 - отрицательное, 1 - положительное или 0
     return int(math.copysign(1, x))
 
 def f(x): # исходная функция
-  return math.tan(x) - 1/x  
+  return Decimal(math.tan(x)) - Decimal(1)/Decimal(x)  
 
 def bisect(a, b, eps): # метод бисекций
    
     k = 0
     x0 = 0
-    an = a
+    an = Decimal(a)
     bn = b
     r = f(a)
     while True:
-        x0 = 0.5*(an + bn)
-        y = f(x0)
-        if ((y == 0) or ((bn - an) <= (2*eps))):
-            return [x0, k]
-   
+        x0 = Decimal(0.5)*Decimal(an + bn)
+        y = Decimal(f(x0))
+        if ((y == 0) or (Decimal(bn - an) < Decimal(2*eps))):
+            return [Decimal(x0), k]
         k = k+1
     
     
         if (sign(y) != sign(r)):
-            bn = x0
+            bn = Decimal(x0)
         else:
-            an = x0
-            r = y   
+            an = Decimal(x0)
+            r = Decimal(y)   
 
 if __name__ == "__main__":
-   
-    answer = bisect(-1, -1/2, 5e-3)
-    print("Корень уравнения tan(x) - 1/x найден по методу бисекций и равен:", answer[0], "\nКоличество итераций равно", answer[1])
+    answer, count = bisect(Decimal(-math.pi/3), Decimal(-math.pi/6), Decimal(5**(-17)))
     
- 
-    
+    answer = Decimal(answer).quantize(Decimal('.00000000000000001'))
+    print("Корень уравнения tan(x) - 1/x найден по методу бисекций и равен:", Decimal(answer), "\nКоличество итераций равно", count)
     
             
         
