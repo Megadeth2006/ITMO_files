f7c4f077a7d7607aa011d06c54b374662ae1c514
Megadeth2006 <danielgrigorevdlol@gmail.com>
2025-04-24 15:56:37 +0300

    new commit

--- BasicsOfProfessionalActivity/lab5/assembler.asm
+++ BasicsOfProfessionalActivity/lab5/assembler.asm
@@ -1,9 +1,6 @@
-ORG 0x010
-RES_ADRESS: WORD 0x5f7
-EOF: WORD 0x0d
-TMP: WORD ?
-
 ORG 0x0b6
+ADR: WORD 0x5f7
+
 START: CLA
 
 S1: IN 5
@@ -11,29 +8,28 @@ S1: IN 5
     BEQ S1
 
     IN 4
-    SWAB
-    ST (RES_ADRESS)
-
-    SWAB
-    CMP EOF
-    BEQ STOP
+    CMP #0x0D
+    BEQ END1
+    ST (ADR)
 
 S2: IN 5
     AND #0x40
     BEQ S2
 
     IN 4
-    ST TMP
-    ADD (RES_ADRESS)
-    ST (RES_ADRESS)+
-
-    LD TMP
-    CMP EOF
-    BEQ STOP
-
+    CMP #0x0D
+    BEQ END2
+    SWAB
+    ADD (ADR)
+    ST (ADR)+
     JUMP S1
-STOP: HLT
-
+    
+END1:
+      JUMP SAVE
+END2:  SWAB
+ADD (ADR)
+SAVE:    ST (ADR)
+ 		HLT 
 ORG 0x5f7
 RES: WORD ?
 
